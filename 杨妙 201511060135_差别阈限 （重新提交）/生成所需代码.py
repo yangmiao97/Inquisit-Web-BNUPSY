36d7254445d00e7e02ed445d99b9a1e1a41686a3
yangmiao97 <907728543@qq.com>
2017-01-26 15:29:42 +0800

    Add files via upload

---
+++ 杨妙 201511060135_差别阈限 （重新提交）/生成所需代码.py
@@ -0,0 +1,30 @@
+# !/usr/bin/evn python
+# _*_ encoding:utf-8_*_
+# -*- coding: utf-8 -*-
+
+'this is a test module'
+
+__author__ = 'Hang Zhang'
+
+import os
+
+work_dir = "D:\work\Python\\picture\\"
+
+os.getcwd()
+
+os.chdir(work_dir)
+
+# 遍历两个需要扫描的文件夹，并将文件名导入到列表当中
+
+NPicNames = os.listdir(work_dir+"NPic\\")
+
+# 打印出inquisit所需要的格式
+
+print("<item NR>")
+for i in range(0, len(NPicNames)):
+    j = i+1
+    print('<picture'+'  '+'N'+str(j)+'>'+'\n')
+    print('   '+'/'+str(j)+' = ' + '"' + NPicNames[i] + '"'+'\n')
+    print("</picture>")
+print("\n")
+
